941992144b3282aa300b9a5b5e25b1e15b066bef
mateujcic <mu8687@rit.edu>
2021-12-18 11:29:23 +0100

    Style navbar Fixes #2

--- portfolio/src/components/Navbar/Navbar.js
+++ portfolio/src/components/Navbar/Navbar.js
@@ -1,13 +1,30 @@
 import {Nav, NavLink} from "../../style/NavStyle.js";
+import "../../style/NavStyle.scss";
+import React, { useEffect, useState } from "react";
+
 
 export default function Navbar() {
+
+  const [scroll, setScroll] = useState(false);
+ useEffect(() => {
+   window.addEventListener("scroll", () => {
+     setScroll(window.scrollY > 50);
+   });
+ }, []);
+
   return (
-    <Nav>
-        <NavLink to="/" activeStyle>
+    <Nav className={scroll && "nav-scrolled"}>
+        <NavLink to="/" >
           Home
         </NavLink>
-        <NavLink to="/projects" activeStyle>
+        <NavLink to="#projects" >
           Projects
+        </NavLink>
+        <NavLink to="#contact" >
+          Contact
+        </NavLink>
+        <NavLink to="/resume" >
+          Resume
         </NavLink>
     </Nav>
   );
--- portfolio/src/style/NavStyle.js
+++ portfolio/src/style/NavStyle.js
@@ -2,19 +2,23 @@ import styled from "styled-components";
 import { NavLink as Link } from "react-router-dom";
 
 export const Nav = styled.nav`
-  height: 7rem;
-  display: flex;
-  justify-content: left;
+  height: 4.5rem;
   z-index: 12;
   position: fixed;
   top: 0;
   width: 100%;
-  background: #1b263b;
-  padding-left: 3rem;
+  background: rgb(246, 251, 255);
+  display:flex;
+  justify-content: flex-end;
+  transition: all 0.5s;
+  border-radius:0.5rem;
+
 `;
 
+
+
 export const NavLink = styled(Link)`
-  color: white;
+  color: grey;
   display: flex;
   align-items: center;
   text-decoration: none;
